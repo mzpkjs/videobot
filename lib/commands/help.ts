2f11517c44632d9d23ccd4daad0b552518c58c16
Mateusz Pietrzak <mateusz@mzpkjs.com>
2024-09-07 18:40:37 +0200

    :tada:

--- lib/commands/help.ts
+++ lib/commands/help.ts
@@ -7,7 +7,7 @@ import { Command, description, name, registered } from "#/core"
 )
 class Help implements Command {
   async execute() {
-    console.native("| Command\t\t\t\t| Description\n")
+    console.log("| Command\t\t\t\t| Description\n")
     const [ invokedCommand ] = process.argv.slice(2)
     const helpful = registered
       .filter(command =>
@@ -16,14 +16,14 @@ class Help implements Command {
       if (command.name === "help") {
         continue
       }
-      console.native(`  ${command.name?.padEnd(16, " ")}\t\t\t  ${command.description}`)
+      console.log(`  ${command.name?.padEnd(16, " ")}\t\t\t  ${command.description}`)
       for (const option of command.options ?? []) {
         const flags = [ option.alias && `-${option.alias}`, option.name && `--${option.name}` ]
           .filter(flag => !!flag)
           .join(", ")
-        console.native(`\t${flags.padEnd(24, " ")}\t\t  ${option.description}`)
+        console.log(`\t${flags.padEnd(24, " ")}\t\t  ${option.description}`)
       }
-      console.native("\n")
+      console.log("\n")
     }
   }
 }
